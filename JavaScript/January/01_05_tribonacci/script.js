9495a388f22c33730d540b49c047e2f1c2ff68e5
AJ Massock <amassock@AJs-MacBook-Pro-2.local>
2023-01-05 19:19:13 -0600

    cleanup code

--- JavaScript/January/01_05_tribonacci/script.js
+++ JavaScript/January/01_05_tribonacci/script.js
@@ -1,13 +1,24 @@
 function tribonacci(signature,n){
-    if (n===0) return []; //short circuit on 0
-    let outArray = [...signature]; //start the sequence
-    for (let i=3; i<n; i++){
-        outArray.push(outArray[i-3]+outArray[i-2]+outArray[i-1])
-    }
-    if (n<=3){ //return the right # if it is below 4
-        return outArray.slice(0,n);
-    }
-     return outArray //otherwise smooth sailing
+    if(n===0) {
+      
+        return [];
+        
+        }
+      
+    let outArray = [...signature];
+    
+    for(let i=3; i<n; i++) {
+      
+          outArray.push(outArray[i-3]+outArray[i-2]+outArray[i-1])
+      
+      }
+    
+      if(n<=3) { 
+        
+          return outArray.slice(0,n);
+        
+      }
+       return outArray
    }
    
    tribonacci([1,2,3],20);
