b46cdd0e6b97c3359ce5695207c068f0e8434967
AJ <ajmassock@gmail.com>
2024-01-21 22:00:59 -0600

    complete challenge

--- JavaScript/2024/January/01_21_Total_Points/script.js
+++ JavaScript/2024/January/01_21_Total_Points/script.js
@@ -1,3 +1,15 @@
 function points(games) {
-  return 0;
+  let points = 0;
+  games.map((games) => {
+    let arr = games.split(":");
+    if (arr[0] > arr[1]) {
+      return (points += 3);
+    } else if (arr[0] < arr[1]) {
+      return points;
+    } else if (arr[0] == arr[1]) {
+      return (points += 1);
+    }
+    return points;
+  });
+  return points;
 }
