52d1c6ea0266f784c598df5fddba43338f6a76d4
AJ <ajmassock@gmail.com>
2024-01-23 21:48:50 -0600

    complete challenge

--- JavaScript/2024/January/01_23_Prime/script.js
+++ JavaScript/2024/January/01_23_Prime/script.js
@@ -1,3 +1,8 @@
 function primeFactors(n) {
   //your code here
+  for (var s = "", d = 2; n > 1; d++) {
+    for (var k = 0; n % d == 0; k++, n /= d);
+    s += k ? (k == 1 ? `(${d})` : `(${d}**${k})`) : "";
+  }
+  return s;
 }
