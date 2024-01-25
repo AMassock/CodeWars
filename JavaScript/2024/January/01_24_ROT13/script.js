3937dde3231cec5f954bcd30eeeb4a444531a3db
AJ <ajmassock@gmail.com>
2024-01-24 22:41:13 -0600

    complete challenge

--- JavaScript/2024/January/01_24_ROT13/script.js
+++ JavaScript/2024/January/01_24_ROT13/script.js
@@ -1,1 +1,11 @@
-function rot13(str) {}
+function rot13(str) {
+  let s1 = "abcdefghijklmnopqrstuvwxyz";
+  let s2 = "NOPQRSTUVWXYZABCDEFGHIJKLM".toLowerCase();
+  const replaced = str.replace(/[a-z]/gi, (v) => {
+    let upper = v === v.toUpperCase();
+    return upper
+      ? s2[s1.indexOf(v.toLowerCase())].toUpperCase()
+      : s2[s1.indexOf(v.toLowerCase())].toLowerCase();
+  });
+  return replaced;
+}
