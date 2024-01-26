050f5d7802d40857a1da8948b494548b1459201a
AJ <ajmassock@gmail.com>
2024-01-25 20:58:50 -0600

    comment

--- JavaScript/2024/January/01_25_Can_Loop/script.js
+++ JavaScript/2024/January/01_25_Can_Loop/script.js
@@ -1,6 +1,10 @@
 function loop_size(node) {
-  var a = node;
-  var b = node;
+  // Find a point in the loop.  Any point will do!
+  // Since the rabbit moves faster than the turtle
+  // and the kata guarantees a loop, the rabbit will
+  // eventually catch up with the turtle.
+  var a = node; // turtle
+  var b = node; // rabbit
 
   do {
     a = a.getNext();
@@ -8,9 +12,19 @@ function loop_size(node) {
   } while (a != b);
   var count = 0;
   do {
+    // The turtle and rabbit are now on the same node,
+    // but we know that node is in a loop.  So now we
+    // keep the turtle motionless and move the rabbit
+    // until it finds the turtle again, counting the
+    // nodes the rabbit visits in the mean time.
     ++count;
     b = b.getNext();
   } while (a != b);
 
   return count;
 }
+
+do {
+  turtle = turtle.getNext();
+  rabbit = rabbit.getNext().getNext();
+} while (turtle != rabbit);
