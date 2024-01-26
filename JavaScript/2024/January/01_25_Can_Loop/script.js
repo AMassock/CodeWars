e71b606ce7cc1e2f9cfc833ebd40ed170d165368
AJ <ajmassock@gmail.com>
2024-01-25 20:57:09 -0600

    complete challenge

--- JavaScript/2024/January/01_25_Can_Loop/script.js
+++ JavaScript/2024/January/01_25_Can_Loop/script.js
@@ -1,1 +1,16 @@
-function loop_size(node) {}
+function loop_size(node) {
+  var a = node;
+  var b = node;
+
+  do {
+    a = a.getNext();
+    b = b.getNext().getNext();
+  } while (a != b);
+  var count = 0;
+  do {
+    ++count;
+    b = b.getNext();
+  } while (a != b);
+
+  return count;
+}
