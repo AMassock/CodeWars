8ea373cfa27d1f5d394cb2dc1d417209ceef8f3a
AJ <ajmassock@gmail.com>
2024-01-26 14:49:24 -0600

    complete challenge

--- JavaScript/2024/January/01_26_Morse_Code_Pt_1/script.js
+++ JavaScript/2024/January/01_26_Morse_Code_Pt_1/script.js
@@ -1,4 +1,15 @@
-decodeMorse = function (morseCode) {
+decodeMorse = (morseCode) => {
   // Your code here
   // You can use MORSE_CODE[morse]
+  return morseCode
+    .split("   ") // get word code, 3 spaces apart
+    .map(
+      (word) =>
+        word
+          .split(" ") // get character code, 1 spaces apart
+          .map((character) => MORSE_CODE[character]) // decode character
+          .join("") // join characters to word
+    )
+    .join(" ") // add spaces between words
+    .trim();
 };
