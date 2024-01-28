14c2b584793e7e6ca63157ab8e8e49773ca8efc6
AJ <ajmassock@gmail.com>
2024-01-27 22:04:00 -0600

    complete challenge:

--- JavaScript/2024/January/01_27_Human_Readable/script.js
+++ JavaScript/2024/January/01_27_Human_Readable/script.js
@@ -1,3 +1,12 @@
 function humanReadable(seconds) {
-  return "";
+  var hours = Math.floor(seconds / 3600);
+  var minutes = Math.floor((seconds % 3600) / 60);
+  var remainingSeconds = seconds % 60;
+
+  var formattedHours = hours < 10 ? "0" + hours : hours;
+  var formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+  var formattedSeconds =
+    remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
+
+  return formattedHours + ":" + formattedMinutes + ":" + formattedSeconds;
 }
