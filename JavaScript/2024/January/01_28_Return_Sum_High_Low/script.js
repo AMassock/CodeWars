521f16c764401c705ca25401c9c28ef022b9a39a
AJ <ajmassock@gmail.com>
2024-01-28 22:58:43 -0600

    finish challenge

--- JavaScript/2024/January/01_28_Return_Sum_High_Low/script.js
+++ JavaScript/2024/January/01_28_Return_Sum_High_Low/script.js
@@ -1,1 +1,8 @@
-function sumArray(array) {}
+function sumArray(array) {
+  // return 0 if array null or empty
+  if (!Array.isArray(array)) return 0;
+  return array
+    .sort((a, b) => a - b)
+    .slice(1, array.length - 1)
+    .reduce((a, b) => a + b, 0);
+}
