d46ed43c71c6cda4ab46b9f97494dac2005329a5
AJ <ajmassock@gmail.com>
2024-02-14 23:42:43 -0600

    complete challenge

--- JavaScript/2024/Febuary/02_14_Multi/script.js
+++ JavaScript/2024/Febuary/02_14_Multi/script.js
@@ -1,3 +1,14 @@
 function multiply(number) {
   //your code here
+  let numStr = number.toString();
+  let numLength = numStr.length;
+
+  if (number <= -1) {
+    return number * 5 ** (numLength - 1);
+  }
+
+  return number * 5 ** numLength;
 }
+
+// return numLength <= -1 ? expr ** (numLength - 1) : expr ** numLength
+// return number * Math.pow(5, Math.abs(number).toString().length);
