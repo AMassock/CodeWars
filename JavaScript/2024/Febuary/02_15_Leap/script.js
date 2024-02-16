8d0aef0d2ae8e64c063a8e07ea388f93d873bcf8
AJ <ajmassock@gmail.com>
2024-02-16 00:22:32 -0600

    complete challenge

--- JavaScript/2024/Febuary/02_15_Leap/script.js
+++ JavaScript/2024/Febuary/02_15_Leap/script.js
@@ -1,3 +1,8 @@
 function yearDays(year) {
   //your code here
+  const leap = new Date(year, 1, 29).getDate() === 29;
+  if (leap || year == 0) {
+    return `${year} has 366 days`;
+  }
+  return `${year} has 365 days`;
 }
