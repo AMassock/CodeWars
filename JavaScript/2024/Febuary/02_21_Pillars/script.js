0bf3a9b03f52f3e4c3dab50c5de32abf2043b060
AJ <ajmassock@gmail.com>
2024-02-21 22:57:26 -0600

    complete challenge

--- JavaScript/2024/Febuary/02_21_Pillars/script.js
+++ JavaScript/2024/Febuary/02_21_Pillars/script.js
@@ -1,3 +1,6 @@
 function pillars(numPill, dist, width) {
   // your code here
+  // find distance between first and last pillar
+  // dist = (pillDist + pillWidth) * numPill
+  return numPill > 1 ? (numPill - 1) * dist * 100 + (numPill - 2) * width : 0;
 }
