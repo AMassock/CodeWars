6883570beb2cabbaf8ffcf4043e8f254017ce35f
AJ <ajmassock@gmail.com>
2024-02-28 23:30:27 -0600

    test solution

--- JavaScript/2024/Febuary/02_26_Arr_plus_arr/script.js
+++ JavaScript/2024/Febuary/02_26_Arr_plus_arr/script.js
@@ -1,3 +1,11 @@
 function arrayPlusArray(arr1, arr2) {
-  return arr1 + arr2; //something went wrong
+  let newArr = [];
+  arr1.forEach((arr2) => {
+    arr2.forEach(() => {
+      let sum = arr1[i] + arr2[i];
+      return sum;
+    });
+    return sum;
+  });
+  return sum; //something went wrong
 }
